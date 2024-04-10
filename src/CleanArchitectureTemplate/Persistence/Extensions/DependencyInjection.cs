4866fa3c9f60746e215b73dae55d3f1aaf757fac
NinPhi <melmarka4@gmail.com>
2024-04-10 18:18:01 +0600

    registered sample repository

--- src/CleanArchitectureTemplate/Persistence/Extensions/DependencyInjection.cs
+++ src/CleanArchitectureTemplate/Persistence/Extensions/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Domain.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
 
 namespace Persistence.Extensions;
 
@@ -9,12 +11,21 @@ public static class DependencyInjection
         this IServiceCollection services, IConfiguration configuration)
     {
         return services
-            .RegisterDbContext(configuration);
+            .RegisterDbContext(configuration)
+            .RegisterRepositories();
     }
 
     private static IServiceCollection RegisterDbContext(
         this IServiceCollection services, IConfiguration configuration)
     {
+        var connection = configuration.GetConnectionString(nameof(AppDbContext));
+
         return services.AddDbContext<AppDbContext>();
+    }
+
+    private static IServiceCollection RegisterRepositories(
+        this IServiceCollection services)
+    {
+        return services.AddTransient<ISampleRepository, SampleRepository>();
     }
 }
